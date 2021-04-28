d665f090ac60e9205b51ecc184db8035d5c17174
Đặng Xuân Danh <57908643+DangXuanDanh@users.noreply.github.com>
2021-04-28 23:01:10 +0700

    Add files via upload

--- src/pkg20424095_slangdictionary/Main.java
+++ src/pkg20424095_slangdictionary/Main.java
@@ -20,6 +20,7 @@ public class Main {
     final static String filePath
         = "slang.txt";
     static HashMap<String, String> slangdictionary = new HashMap<String, String>();
+    static HashMap<String, String> slanghistory = new HashMap<String, String>();
     private static Scanner x;
 
     /**
@@ -27,47 +28,56 @@ public class Main {
      */
     public static void main(String[] args) throws IOException{
         // TODO code application logic here
-        //SlangWord();
-        //SearchDefinition();
-        //AddSlangWord();
-        //DeleteSlangWord();
-        //EditSlangWord();
-        RandomSlangWord();
-        /*DataInputStream dis;
+        
+        DataInputStream dis;
         Scanner sc = new Scanner(System.in);
         int luachon = 1;
-        while(luachon <= 11){
-        System.out.println("1.Tìm kiếm theo slang word");
-        System.out.println("2.Tìm kiếm theo definition");
-        System.out.println("3.Hiển thị history");
-        System.out.println("4.Add 1 slang words");
-        System.out.println("5.Edit 1 slang word");
-        System.out.println("6.Delete 1 slang word");
-        System.out.println("7.Reset danh sách slang words gốc.");
-        System.out.println("8.Random 1 slang word");
-        System.out.println("9.Đố vui slang word");
-        System.out.println("10.Đố vui definition");
+        while(luachon <= 10){
+        System.out.println("1.Tìm kiếm theo slang word || 2.Tìm kiếm theo definition || 3.Hiển thị history ||");
+        System.out.println("4.Add 1 slang words || 5.Edit 1 slang word || 6.Delete 1 slang word ||");
+        System.out.println("7.Reset danh sách slang words gốc || 8.Random 1 slang word ||");
+        System.out.println("9.Đố vui slang word || 10.Đố vui definition ||");
         System.out.println("11.Thoat");
         System.out.print("Nhập lựa chọn: ");
         luachon = Integer.parseInt(sc.nextLine());
         
         switch (luachon) {
         case 1:
-            ;
+            SlangWord();
+            SearchSlangWord();
             continue;
         case 2:
-            ;
+            SlangWord();
+            SearchDefinition();
             continue;
         case 3:
-            ;
+            ShowHistory();
             continue;
         case 4:
-            ;
+            AddSlangWord();
+            continue;
+        case 5:
+            EditSlangWord();
+            continue;
+        case 6:
+            DeleteSlangWord();
+            continue;
+        case 7:
+            System.out.println("Coming soon!!!");
+            continue;
+        case 8:
+            RandomSlangWord();
+            continue;
+        case 9:
+            System.out.println("Coming soon!!!");
+            continue;
+        case 10:
+            System.out.println("Coming soon!!!");
             continue;
         default:
             break;
         }
-        }*/
+        }
     }
     
     public static void SlangWord() throws IOException{
@@ -86,7 +96,7 @@ public class Main {
 			slangdictionary.put(parts[0], parts[1]);
 
            }       
-            
+        br.close();
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -95,22 +105,77 @@ public class Main {
     }
     
     public static void SearchSlangWord() throws IOException{
+        File SlangHistory = new File("history.txt");
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in, "utf8"));
+        BufferedWriter bw = new BufferedWriter( new FileWriter(SlangHistory,true));
+        PrintWriter pw = new PrintWriter(bw);
+        
         System.out.print("Nhập Slang Word cần tìm:");
         String slangword = sc.nextLine();
         
         System.out.println("definition : "+slangdictionary.get(slangword));
-        
+        pw.println(slangword+"`"+slangdictionary.get(slangword));
+        pw.flush();
+        pw.close();
     }
     
     public static void SearchDefinition() throws IOException{
+        File SlangHistory = new File("history.txt");
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in, "utf8"));
+        BufferedWriter bw = new BufferedWriter( new FileWriter(SlangHistory,true));
+        PrintWriter pw = new PrintWriter(bw);
+        
         System.out.print("Nhập definition cần tìm:");
         String definition = sc.nextLine();
         for(Entry<String, String> entry: slangdictionary.entrySet()) {
             //System.out.println(entry.getValue());
             if(entry.getValue().contains(definition)) {
-                System.out.println("Slang Word : "+entry.getKey());
+                System.out.println("Slang Word : "+entry.getKey()+", Definition: "+ slangdictionary.get(entry.getKey()));
+                pw.println(entry.getKey()+"`"+slangdictionary.get(entry.getKey()));
+            }   
         }
+        pw.flush();
+        pw.close();
+        
     }
+    
+    public static void ShowHistory() throws IOException{
+        File SlangHistory = new File("history.txt");
+        String slangword=""; String definition="";
+        String Confirm = "";
+        System.out.println("---------Hitory Search-----"); 
+         
+         try{
+            x = new Scanner (new File("history.txt"));
+            x.useDelimiter("[`\n]");
+             while (x.hasNext()) {
+                slangword=x.next();
+                definition = x.next();
+                               
+                System.out.println("Slang word: "+slangword+ ", Definition: "+definition);
+                //JOptionPane.showMessageDialog(null,"Ho Ten: "+hoten+",Diem: "+Diem+",Dia Chi: "+DiaChi+",Ghi Chu: "+Ghichu);
+             }
+            x.close();
+            while(!Confirm.equals("y")){
+            System.out.print("Bạn muốn xóa lịch sử(y/n):");
+            Confirm = sc.nextLine();
+            if(Confirm.equals("y")){
+                if(SlangHistory.delete()){
+                    System.out.println("Lịch sử search đã bị xóa!!!");
+                }
+                else{
+                    System.out.println("failed");
+                }
+            }
+            else if(Confirm.equals("n")){
+                break;
+            }
+            }
+                
+        }catch(Exception e){
+            System.out.println("Hiện không có lịch sử search");
+        }
+        
         
     }
     
@@ -165,7 +230,7 @@ public class Main {
             FileReader fr = new FileReader(filePath);
             BufferedReader br = new BufferedReader(fr);
             
-            System.out.print("Nhap SlangWord muon xoa: ");
+            System.out.print("Nhập SlangWord muốn xóa: ");
             removeTemp =sc.nextLine();
             while(Confirm != "y"){
                 System.out.print("Bạn có muốn xóa Slang Word(" + removeTemp +")không(y/n):");
@@ -190,7 +255,7 @@ public class Main {
                         count = 1;
                     }
                 }
-                sc.close();
+                
                 pw.flush();
                 pw.close();
                 fr.close();
@@ -213,7 +278,7 @@ public class Main {
             System.out.println("Finished ! Check your file !");
         }
         else if(count == 0){
-            System.out.println("Khong tim thay Slangword " + removeTemp);
+            System.out.println("Không tìm thấy Slangword " + removeTemp);
         }
     }
     
@@ -237,7 +302,7 @@ public class Main {
             FileReader fr = new FileReader(filePath);
             BufferedReader br = new BufferedReader(fr);
             
-            System.out.print("Nhap Slangword muon sua: ");
+            System.out.print("Nhập Slangword muốn sửa: ");
             editTemp =sc.nextLine();
             
             
@@ -252,10 +317,10 @@ public class Main {
                 data = currentLine.split("`");
                 if((data[0].equalsIgnoreCase(editTemp)))
                 {
-                    System.out.print("Nhap Slangword:");
+                    System.out.print("Nhập Slangword:");
                     String newSlangWord = sc.nextLine();
             
-                    System.out.print("Nhap Definition:");
+                    System.out.print("Nhập Definition:");
                     String newDefinition = sc.nextLine();
                     
                     pw.println(newSlangWord+"`"+newDefinition);
@@ -267,7 +332,7 @@ public class Main {
             }
             
             
-            sc.close();
+            //sc.close();
             pw.flush();
             pw.close();
             fr.close();
@@ -282,7 +347,7 @@ public class Main {
             System.out.println("");
         }
         if(count == 0){
-            System.out.println("Khong tim thay Slangword " + editTemp);
+            System.out.println("Không tìm thấy Slangword " + editTemp);
         }
         else{
             System.out.println("Finished ! Check your file !");
